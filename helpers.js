3fa2e5fc1c2c1369a619b5f6fa4e88afff284066
Charles C. Lee <charleschanlee@gmail.com>
2015-06-25 20:55:59 -0700

    added bullets no interaction yet

--- helpers.js
+++ helpers.js
@@ -1,8 +1,23 @@
 // Helper functions
 
+
+// Bullet
+function Bullet(x, y, vely, w, h, color) {
+  this.x = x;
+  this.y = y;
+  this.vely = vely;
+  this.width = w;
+  this.height = h;
+  this.color = color;
+}
+
+Bullet.prototype.update = function () {
+  this.y += this.vely;
+};
+
 // Screen
 function Display(width, height) {
-  this.canvas = document.createElement("canvas");
+  this.canvas = document.createElement('canvas');
   this.canvas.width = this.width = width;
   this.canvas.height = this.height = height;
   this.context = this.canvas.getContext("2d");
@@ -12,6 +27,28 @@ function Display(width, height) {
 
 Display.prototype.drawSprite = function (sp, x, y) {
   this.context.drawImage(sp.image, sp.x, sp.y, sp.w, sp.h, x, y, sp.w, sp.h);
+};
+
+Display.prototype.drawBullet = function (bullet) {
+  this.context.fillStyle = bullet.color;
+  this.context.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+};
+
+Display.prototype.endGame = function () {
+
+  var canvas = document.getElementsByTagName("canvas")[0];
+  canvas.style.backgroundColor = '#C0392B';
+
+  // var context = canvas.getContext('2d');
+  // var x = canvas.width / 2;
+  // var y = canvas.height / 2;
+  //
+  // context.font="12px Arial";
+  // context.fillStyle='white';
+  // context.textAlign = 'center';
+  // context.fillText("Hello World!", x, y);
+
+  // context.fillText('Hello World!', x, y);
 };
 
 Display.prototype.clear = function () {
